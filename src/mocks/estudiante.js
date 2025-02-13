198ad7acec343ea8a6c4bf2605cd4cbcda0b1403
Salvador Ahumada <ahumada1790@gmail.com>
2025-02-12 18:05:17 -0600

    feat: Se agrega pdfmake para general pdfs dinamicos con información real.

---
+++ src/mocks/estudiante.js
@@ -0,0 +1,46 @@
+const estudiante = {
+        "escuela": "CETMAR 6",
+        "estudiante": "Laura Vargas",
+        "grupo": "1A",
+        "celular": "0123456789",
+        "materia": "Ofimática",
+        "general": {
+            "total": 5,
+            "asistencias": 5,
+            "faltas": 0,
+            "tareasEntregadas": 3,
+            "tareasNoEntregadas": 2,
+            "tareasOtroDia": 5
+        },
+        "materias": {
+            "data": [
+                {
+                    "horario": "07:00 - 07:50",
+                    "materia": "Matemáticas",
+                    "docente": "Juan Pérez",
+                    "asistencia": true,
+                    "entrega_tarea": true,
+                    "asignacion_tarea": true
+                },
+                {
+                    "horario": "07:50 - 08:40",
+                    "materia": "Física",
+                    "docente": "Juan Pérez",
+                    "asistencia": true,
+                    "entrega_tarea": true,
+                    "asignacion_tarea": true
+                },
+                {
+                    "horario": "09:30 - 10:00",
+                    "materia": "Física",
+                    "docente": "María López",
+                    "asistencia": true,
+                    "entrega_tarea": true,
+                    "asignacion_tarea": true
+                }
+            ],
+            "error": null
+        }
+    }
+
+export default estudiante;
--- src/reports/reporte_diario.js
+++ src/reports/reporte_diario.js
@@ -1,73 +1,95 @@
+/**
+ * TODO: Verificar valores de tarea/tareaOtrodia
+ * TODO: Verificar si se puede enviar el pdf en Blob
+ */
 import { configMessage } from "../config/configMessage.js";
 import { supabase } from "./../config/clientSupabase.js";
+import general from '../mocks/general.js';
+import estudiante from "../mocks/estudiante.js";
+import { generatePdf } from "./pdfmake.js";
+// Enviar 'true' para mockear y no hacer llamadas innecesarias a supabase.
+const MOCK = true;
+
+const mockSupabase = async (mock, type, args) => {
+  if (!mock) {
+    let { data, error } = await supabase.rpc(type, args);
+    return { data, error }
+  }
+  const response = {
+    get_datos_generales: general,
+    get_datos_detallados: estudiante
+  }
+  return { data: response[type], error: null }
+}
+
+const mostrarDocs = async (doc) => {
+  doc.getBlob(blob => {
+    const blobUrl = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = blobUrl;
+    a.innerText = 'descargar';
+    a.download = "example.pdf";
+    document.body.appendChild(a);
+    document.querySelector('#links').appendChild(a);
+  });
+
+}
+
+const validarKey = (obj, key) => {
+  if (obj[key]) return obj[key];
+  return `${key} no disponible`;
+}
+
+const getMaterias = ({ materias }) => {
+  return materias.data.reduce((acc, curr) => {
+    acc[curr.horario] = {
+      docente: validarKey(curr, 'docente'),
+      materia: validarKey(curr, 'materia'),
+      alumno: {
+        asistencia: validarKey(curr, 'asistencia'),
+        tarea: validarKey(curr, 'entrega_tarea'),
+        tareasOtroDia: validarKey(curr, 'asignacion_tarea')
+      }
+    }
+    return acc;
+  }, {})
+}
 
 const handleGenerales = async (p_fecha) => {
   const urlPDF = "https://www.colonialtours.com/ebook/ebooks/ElPrincipito.pdf";
   try {
-    let { data, error } = await supabase.rpc("get_datos_generales", {
-      p_fecha,
-    });
+    let { data, error } = await mockSupabase(MOCK, "get_datos_generales", { p_fecha });
 
     if (error) console.error(error);
     else console.log(data);
 
     for (const student of data) {
-      const detailedData = await handleDetallados(p_fecha, student?.matricula);
-      const pdfData = {
-        escuela: "CETMAR 6",
-        estudiante: student.alumno,
-        grupo: student.grupo,
-        celular: student.celular,
-        materia: student.carrera || "Ofimática",
+      const detailedData = await mockSupabase(MOCK, "get_datos_detallados", { p_fecha, p_matricula: student?.matricula });
+      const estudiante = {
+        escuela: validarKey(detailedData.data, 'escuela'),
+        estudiante: validarKey(student, 'alumno'),
+        grupo: validarKey(student, 'grupo'),
+        materia: validarKey(detailedData.data, 'materia'),
         general: {
-          total: student.total_asistencias,
-          asistencias: student.asistencias,
-          faltas: student.faltas,
-          tareasEntregadas: student.tareas_entregadas,
-          tareasNoEntregadas: student.tareas_no_entregadas,
-          tareasOtroDia: student.total_tareas,
+          total: validarKey(student, 'total_asistencias'),
+          asistencias: validarKey(student, 'asistencias'),
+          faltas: validarKey(student, 'faltas'),
+          tareasEntregadas: validarKey(student, 'tareas_entregadas'),
+          tareasNoEntregadas: validarKey(student, 'tareas_no_entregadas'),
+          tareasOtroDia: validarKey(student, 'total_tareas')
         },
-        materias: detailedData,
-      };
-
-      console.log(pdfData);
-      // configMessage(student.celular, student.alumno, urlPDF, p_fecha); --> Envio de whatsapp con información de la bd
-      configMessage('3114000218', "Carlos Amezcua", urlPDF, p_fecha);
+        materias: getMaterias(detailedData.data)
+      }
+      const pdfDoc = await generatePdf(estudiante);
+      pdfDoc.getBlob((blob) => {
+        const pdfFile = new File([blob], `${estudiante.estudiante.replace(' ', '_').toLowerCase()}.pdf`, { type: "application/pdf" });
+        console.log(pdfFile)
+        // configMessage(student.celular, student.alumno, urlPDF, p_fecha); --> Envio de whatsapp con información de la bd
+        // configMessage('3114000218', "Carlos Amezcua", urlPDF, p_fecha);
+      })
     }
   } catch (error) {
     console.error("Error", error);
-    return null;
-  }
-};
-
-const handleDetallados = async (p_fecha, p_matricula) => {
-  try {
-    let { data, error } = await supabase.rpc("get_datos_detallados", {
-      p_fecha,
-      p_matricula,
-    });
-
-    if (error) {
-      console.error(error);
-      return null;
-    }
-
-    const materias = {};
-    data.forEach((item) => {
-      materias[item.horario] = {
-        MATERIA: item.materia,
-        DOCENTE: item.docente,
-        alumno: {
-          asistencia: item.asistencia,
-          tarea: item.entrega_tarea,
-          tareasOtroDia: item.asignacion_tarea,
-        },
-      };
-    });
-
-    return materias;
-  } catch (error) {
-    console.error(error);
     return null;
   }
 };
